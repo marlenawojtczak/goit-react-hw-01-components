d5f0d2ceb122b73fb430345e7597b6ccb3790e1a
marlenawojtczak <wojtczakmarlena100@gmail.com>
2023-06-08 19:36:03 +0200

    added task1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,55 @@
+import PropTypes from 'prop-types';
+import {
+  ProfileContainer,
+  Avatar,
+  Description,
+  Name,
+  Tag,
+  Location,
+  Stats,
+  StatItem,
+  Label,
+  Quantity,
+} from './Profile.styled';
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <ProfileContainer>
+      <Description>
+        <Avatar src={avatar} alt="User avatar" />
+        <Name>{username}</Name>
+        <Tag>{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
+
+      <Stats>
+        <StatItem>
+          <Label>Followers</Label>
+          <Quantity>{stats.followers}</Quantity>
+        </StatItem>
+        <StatItem>
+          <Label>Views</Label>
+          <Quantity>{stats.views}</Quantity>
+        </StatItem>
+        <StatItem>
+          <Label>Likes</Label>
+          <Quantity>{stats.likes}</Quantity>
+        </StatItem>
+      </Stats>
+    </ProfileContainer>
+  );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default Profile;
---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+
+export const ProfileContainer = styled.div`
+  font-family: 'Lato', sans-serif;
+  font-size: 16px;
+  color: #9aa4b3;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 0 0;
+  margin: 0 auto;
+  box-shadow: 12px 13px 25px -20px rgba(66, 68, 90, 1);
+  border: 1px solid #cfcfcf78;
+  border-radius: 5px;
+  max-width: 300px;
+`;
+
+export const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+  padding: 60px 0;
+`;
+
+export const Avatar = styled.img`
+  width: 90px;
+  padding-bottom: 30px;
+`;
+
+export const Name = styled.p`
+  font-size: 18px;
+  font-weight: 900;
+  color: #000;
+`;
+
+export const Tag = styled.p``;
+
+export const Location = styled.p``;
+
+export const Stats = styled.ul`
+  display: flex;
+  width: 100%;
+`;
+
+export const StatItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  border: 1px solid #e9e8e8;
+  padding: 15px;
+  background-color: #f2f5f9;
+`;
+
+export const Label = styled.span`
+  font-size: 14px;
+`;
+
+export const Quantity = styled.span`
+  font-weight: 900;
+  color: #000;
+`;
